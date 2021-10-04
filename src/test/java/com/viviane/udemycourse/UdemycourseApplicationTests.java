35cda96a52ebbd6fc0d48e16e0796195228bca00
Viviane Lara <46038257+ViFLara@users.noreply.github.com>
2021-10-04 10:22:50 -0300

    project created

---
+++ src/test/java/com/viviane/udemycourse/UdemycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viviane.udemycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
