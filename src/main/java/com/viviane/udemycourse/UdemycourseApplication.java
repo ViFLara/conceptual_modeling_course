54016b82162b8219f8b63994e7bacff6fdbb93fe
Viviane Lara <46038257+ViFLara@users.noreply.github.com>
2021-10-04 23:53:52 -0300

    creating instance operation

--- src/main/java/com/viviane/udemycourse/UdemycourseApplication.java
+++ src/main/java/com/viviane/udemycourse/UdemycourseApplication.java
@@ -1,13 +1,32 @@
 package com.viviane.udemycourse;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.viviane.udemycourse.domain.Category;
+import com.viviane.udemycourse.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class UdemycourseApplication {
+public class UdemycourseApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdemycourseApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
